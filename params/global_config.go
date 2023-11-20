74a3ce332df6489ce305b46f61e368ffa598e4bc
yamasita <596155437@qq.com>
2023-11-20 10:57:25 +0800

    参数修改1120

--- params/global_config.go
+++ params/global_config.go
@@ -3,9 +3,9 @@ package params
 var (
 	Block_Interval      = 5000  // generate new block interval
 	MaxBlockSize_global = 1200  // the block contains the maximum number of transactions
-	InjectSpeed         = 2400  // the transaction inject speed
-	TotalDataSize       = 48000 // the total number of txs
-	BatchSize           = 4800  // supervisor read a batch of txs then send them, it should be larger than inject speed
+	InjectSpeed         = 1200  // the transaction inject speed
+	TotalDataSize       = 24000 // the total number of txs
+	BatchSize           = 2400  // supervisor read a batch of txs then send them, it should be larger than inject speed
 	BrokerNum           = 10
 	NodesInShard        = 4
 	ShardNum            = 4
